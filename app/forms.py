6f4e8dd388472dad9acb31410972963f29dd7d46
Anirudh Ramachandran <anirudhram10298@gmail.com>
2021-03-22 13:59:06 +0530

    Added flashes and validations

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtfforms.validaotrs import Length, EqualTo, Email, DataRequired, ValidationError
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from models import User
 
 class RegisterForm(FlaskForm):
-    username = StringField('User Name', validators = [Length(min =2,max = 30), DataRequired()])
-    email = StringField('Email Address', validators = [Email(), DataRequired()])
-    password1 = PasswordField('Password', validators = [Length(min = 5)], DataRequired())
-    password2 = PasswordField('Confirm Password', validators = [EqualTo('password1'), DataRequired()]) 
-    submit = SubmitField('Create Account')
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(name = username_to_check.data).first()
+        if user:
+            raise ValidationError('Username Already Exists')
+     
+    def validate_email(self, email_to_check):
+        email = User.query.filter_by(email = email_to_check.data).first()
+        if email:
+            raise ValidationError('Email Address already exists')
+  
+    username = StringField(label='User Name', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email Address', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password', validators=[EqualTo('password1'), DataRequired()])
+    submit = SubmitField(label='Create Account')
--- app/market.py
+++ app/market.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 
 @app.route('/')
@@ -14,5 +15,24 @@ def market_page():
     item = Item.query.all()
     return render_template('market.html',items = item)    
 
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    from forms import RegisterForm
+    form = RegisterForm()
+    if form.validate_on_submit():
+        from models import User
+        user_to_create = User(name = form.username.data,
+                              email = form.email_address.data,
+                              password = form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        flash("You have successfully created a new account ")
+        return redirect(url_for('market_page'))
+    if form.errors != {}: 
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+    
+    return render_template('register.html', form=form)
+    
 if __name__ == '__main__':
     app.run(debug=True)
